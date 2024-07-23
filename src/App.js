6b001edf54982a9fa4277f0608f38a490b5b84b9
Anupam Adarsh <anupamadarsh@Anupams-MacBook-Air.local>
2024-07-23 21:58:58 +0530

    work in progress

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, {useState} from 'react';
+
+import send from '../src/assets/send.svg';
+import user from '../src/assets/user.png';
+import loader from '../src/assets/loader.svg';
+
 
 function App() {
     return (
-        <div className="App">
-            hello chatGpt
+        <div className={'chatGPT-app'}>
+            <section className={'chat-container'}>
+                <div className={'layout'}>
+                    <div className={'chat-bubble'}>
+
+                    </div>
+                </div>
+            </section>
+            <footer>
+                <input
+                    className={'composebar'}
+                    autoFocus
+                    type={'text'}
+                    placeholder={'Ask Anything!'}
+                    onChange={() => {
+                    }}
+
+                />
+                <div className={'send-button'}>
+                    <img src={send} alt={'sendBtn'}/>
+                </div>
+            </footer>
         </div>
     );
 }
